fab65a1bf84583fcd0843d4fb74104133b891fea
Hans Reichenbach <hans.reichenbach@okta.com>
2015-03-02 17:27:43 -0800

    WIP convert project format to gradle and android studio layout

--- setup.sh
+++
@@ -1,2 +0,0 @@
-# run this as 'source setup.sh' instead of './setup.sh' since ./ runs its own shell instance then closes, so the export changes aren't kept
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home
